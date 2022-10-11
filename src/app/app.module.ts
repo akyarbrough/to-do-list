29e7981e6ebd542f3f1bfdc049c861f24e4475ac
akyarbrough <ayarbrough0816@gmail.com>
2022-10-10 20:28:20 -0500

    Created ToDo Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoComponent } from './todolist/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodolistComponent,
+    TodoFormComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule
